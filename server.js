008ebf39dcf9870d0a2c228c3d38240c68a9a73f
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-02-28 13:46:57 -0600

    made client build folder static public file

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('/api/test', (req, res) => {
-  res.json({ testData: ['Here is some data', 1234]});
+app.get("/api/test", (req, res) => {
+  res.json({ testData: ["Here is some data", 1234] });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(PORT);
