76593f065f80157067ebbd678a65d455d540cdfc
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-02-28 12:32:28 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json({ testData: ['Here is some data', 1234]});
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(PORT);
+
+console.log(`Server listening on ${PORT}`);
