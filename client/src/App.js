dea8a70cf75aeffbc8a50eb963fef72b7d832078
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-03-05 07:51:50 -0600

    extracted invoice form

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import TextField from "@material-ui/core/TextField";
 import MyDocument from "./components/MyDocument";
 import DateInput from "./components/DateInput";
 import "./App.css";
+import CreateInvoiceForm from "./components/CreateInvoiceForm";
+import { set } from "date-fns";
 
 const App = () => {
   const [lessonNum, setLessonNum] = useState(4);
@@ -32,15 +34,12 @@ const App = () => {
   }
 
   const onDateChange = (date, inputId) => {
-    console.log(inputId);
     let numRegex = /[0-9]+/;
     let result = inputId.match(numRegex);
-    console.log(result[0]);
     updateFieldChanged("date", Number(result[0]), date);
   };
 
   const onCostChange = (event, inputId) => {
-    console.log(event.target.value);
     let numRegex = /[0-9]+/;
     let result = inputId.match(numRegex);
     updateFieldChanged("cost", Number(result[0]), Number(event.target.value));
@@ -67,115 +66,25 @@ const App = () => {
 
   return (
     <div className="container-fluid mt-5">
-      <div className="row">
-        <div className="col-lg-4 col-12">
-          <form onSubmit={(e) => onFormSubmit(e)}>
-            <TextField
-              id="studentNameInput"
-              label="Student Name"
-              value={studentName}
-              onChange={(e) => setStudentName(e.target.value)}
-            />
-            <TextField
-              id="yourNameInput"
-              label="Your Name"
-              value={yourName}
-              onChange={(e) => setYourName(e.target.value)}
-            />
-            <TextField
-              id="parentName"
-              label="Parent Name"
-              value={parentName}
-              onChange={(e) => setParentName(e.target.value)}
-            />
-            <TextField
-              id="parentEmail"
-              label="Parent Email"
-              value={parentEmail}
-              onChange={(e) => setParentEmail(e.target.value)}
-            />
-            <TextField id="invoiceMonth" label="Month" />
-            <TextField
-              id="numLessons"
-              label="Number of Lessons"
-              type="number"
-              value={lessonNum}
-              onChange={(e) => {
-                setLessonNum(e.target.value);
-              }}
-            />
-
-            <div className="row">
-              <div className="col-7">
-                <DateInput id="date-input-0" onDateChange={onDateChange} value={lessons[0].date} />
-              </div>
-              <div className="col-5">
-                <TextField
-                  id="cost-input-0"
-                  label="Amount"
-                  type="number"
-                  value={lessons[0].cost}
-                  onChange={(e) => {
-                    onCostChange(e, e.target.id);
-                  }}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-7">
-                <DateInput id="date-input-1" onDateChange={onDateChange} value={lessons[1].date} />
-              </div>
-              <div className="col-5">
-                <TextField
-                  id="cost-input-1"
-                  label="Amount"
-                  type="number"
-                  value={lessons[1].cost}
-                  onChange={(e) => {
-                    onCostChange(e, e.target.id);
-                  }}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-7">
-                <DateInput id="date-input-2" onDateChange={onDateChange} value={lessons[2].date} />
-              </div>
-              <div className="col-5">
-                <TextField
-                  id="cost-input-2"
-                  label="Amount"
-                  type="number"
-                  value={lessons[2].cost}
-                  onChange={(e) => {
-                    onCostChange(e, e.target.id);
-                  }}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-7">
-                <DateInput id="date-input-3" onDateChange={onDateChange} value={lessons[3].date} />
-              </div>
-              <div className="col-5">
-                <TextField
-                  id="cost-input-3"
-                  label="Amount"
-                  type="number"
-                  value={lessons[3].cost}
-                  onChange={(e) => {
-                    onCostChange(e, e.target.id);
-                  }}
-                />
-              </div>
-            </div>
-            <div>
-              <button className="btn btn-secondary" type="submit">View Pdf</button>
-            </div>
-          </form>
-        </div>
-        <div className="col-lg-8 col-12"></div>
-      </div>
+      <CreateInvoiceForm 
+        onFormSubmit={onFormSubmit} 
+        onDateChange={onDateChange}
+        onCostChange={onCostChange}
+        lessonNum={lessonNum}
+        setLessonNum={setLessonNum}
+        studentName={studentName}
+        setStudentName={setStudentName}
+        yourName={yourName}
+        setYourName={setYourName}
+        parentName={parentName}
+        setParentName={setParentName}
+        parentEmail={parentEmail}
+        setParentEmail={setParentEmail}
+        lessons={lessons}
+        setLessons={setLessons}
+        total={total}
+        setTotal={setTotal}
+        />
     </div>
   );
 };
--- client/src/components/CreateInvoiceForm.js
+++ client/src/components/CreateInvoiceForm.js
@@ -1,12 +1,113 @@
 import React from 'react';
 import TextField from "@material-ui/core/TextField";
+import DateInput from "./DateInput";
 
-const CreateInvoiceForm = () => {
+const CreateInvoiceForm = (props) => {
   return (
-    <form>
-      <TextField id="yourNameInput" label="Your Name" />
-      <TextField id="studentNameInput" label="Student Name" />
-    </form>
+    <form onSubmit={(e) => props.onFormSubmit(e)}>
+            <TextField
+              id="studentNameInput"
+              label="Student Name"
+              value={props.studentName}
+              onChange={(e) => props.setStudentName(e.target.value)}
+            />
+            <TextField
+              id="yourNameInput"
+              label="Your Name"
+              value={props.yourName}
+              onChange={(e) => props.setYourName(e.target.value)}
+            />
+            <TextField
+              id="parentName"
+              label="Parent Name"
+              value={props.parentName}
+              onChange={(e) => props.setParentName(e.target.value)}
+            />
+            <TextField
+              id="parentEmail"
+              label="Parent Email"
+              value={props.parentEmail}
+              onChange={(e) => props.setParentEmail(e.target.value)}
+            />
+            <TextField id="invoiceMonth" label="Month" />
+            <TextField
+              id="numLessons"
+              label="Number of Lessons"
+              type="number"
+              value={props.lessonNum}
+              onChange={(e) => {
+                props.setLessonNum(e.target.value);
+              }}
+            />
+
+            <div className="row">
+              <div className="col-7">
+                <DateInput id="date-input-0" onDateChange={props.onDateChange} value={props.lessons[0].date} />
+              </div>
+              <div className="col-5">
+                <TextField
+                  id="cost-input-0"
+                  label="Amount"
+                  type="number"
+                  value={props.lessons[0].cost}
+                  onChange={(e) => {
+                    props.onCostChange(e, e.target.id);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-7">
+                <DateInput id="date-input-1" onDateChange={props.onDateChange} value={props.lessons[1].date} />
+              </div>
+              <div className="col-5">
+                <TextField
+                  id="cost-input-1"
+                  label="Amount"
+                  type="number"
+                  value={props.lessons[1].cost}
+                  onChange={(e) => {
+                    props.onCostChange(e, e.target.id);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-7">
+                <DateInput id="date-input-2" onDateChange={props.onDateChange} value={props.lessons[2].date} />
+              </div>
+              <div className="col-5">
+                <TextField
+                  id="cost-input-2"
+                  label="Amount"
+                  type="number"
+                  value={props.lessons[2].cost}
+                  onChange={(e) => {
+                    props.onCostChange(e, e.target.id);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-7">
+                <DateInput id="date-input-3" onDateChange={props.onDateChange} value={props.lessons[3].date} />
+              </div>
+              <div className="col-5">
+                <TextField
+                  id="cost-input-3"
+                  label="Amount"
+                  type="number"
+                  value={props.lessons[3].cost}
+                  onChange={(e) => {
+                    props.onCostChange(e, e.target.id);
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <button className="btn btn-secondary" type="submit">View Pdf</button>
+            </div>
+          </form>
   )
 }
 
